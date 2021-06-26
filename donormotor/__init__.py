0a20360c9f8365539c6cede7d352fac686530533
mutantmonkey <mutantmonkey@mutantmonkey.in>
2021-06-25 20:32:35 -0700

    Move donate.views import to fix import issue
    To prevent issues with uninitialized imports, move this import into
    __init__. Otherwise, the donormotor.admin.view_utils import in
    donormotor.donate.views will fail.

--- donormotor/__init__.py
+++ donormotor/__init__.py
@@ -184,6 +184,7 @@ def init_app():
     app.register_blueprint(admin.bp, url_prefix='/admin')
 
     from donormotor import donate
+    from donormotor.donate import views as donate_views
     app.register_blueprint(donate.bp, url_prefix='/donate')
 
     from donormotor import cli
